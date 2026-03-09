0fa2329a18799624ef5173ee3cfeeec72fcc11b9
sojotype <ifweblink@gmail.com>
2026-03-09 14:46:56 +0300

    feat(plugin-frontend): add copyToClipboard utility function

---
+++ packages/plugin/frontend/utils/copy.ts
@@ -0,0 +1,15 @@
+export function copyToClipboard(text: string) {
+  const textarea = document.createElement("textarea");
+  textarea.value = text;
+  textarea.style.position = "fixed";
+  textarea.style.opacity = "0";
+  document.body.appendChild(textarea);
+  textarea.focus();
+  textarea.select();
+
+  try {
+    document.execCommand("copy");
+  } finally {
+    document.body.removeChild(textarea);
+  }
+}
